f6e72078640c0cce5db78b55832da666e5db5531
diaa mohamed <diaa.mohamed@egabi.com>
2025-02-17 17:15:06 +0200

    init the begin of angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from "./components/home/home.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [NavbarComponent, HomeComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'angular_app';
