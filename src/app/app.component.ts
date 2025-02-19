500ded0a2fb1f22d3743ca5e774e22e135425151
diaa mohamed <diaa.mohamed@egabi.com>
2025-02-19 18:43:11 +0200

    add Iproduct interface and Product componnet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
 import { HomeComponent } from "./components/home/home.component";
 import { FooterComponent } from "./components/footer/footer.component";
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
---
+++ src/app/models/iproduct.ts
@@ -0,0 +1,8 @@
+export interface Iproduct {
+    Name:string,
+    Description:string,
+    Price:number,
+    Quantity:number,
+    ImageUrl:string,
+    CategoryId:number
+}
